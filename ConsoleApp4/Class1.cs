b1efc68a4540d203e588f8894d12ed8c7cea4d11
Rawan Eldamanhory <rawaneldamanhory@gmail.com>
2026-01-08 15:18:19 +0200

    Initial commit: OOP bank console application

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Money { get; set; }
+
+        public BankAccount(string xname, double xmoney)
+        {
+            Name = xname;
+            Money = xmoney;
+        }
+        public void Add(double amount)
+        {
+            Console.WriteLine("Add " + amount + " to the account .");
+            Money = Money + amount;
+        }
+        public void Subtract(double amount)
+        {
+            Console.WriteLine("Subtract " + amount + " from the account .");
+            Money = Money - amount;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("NOW " + Name + " has " + Money);
+        }
+    }
+}
+
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc1 = new BankAccount("Ali", 1000);
+            acc1.Add(500);
+            acc1.PrintInfo();
+            acc1.Subtract(200);
+            acc1.PrintInfo();
+        }
+    }
+}
